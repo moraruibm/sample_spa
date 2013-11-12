41d2ce4c4f337d39010184411bb832b858a85e2f
moraruetc <moraru.etc@gmail.com>
2013-11-12 18:07:58 +0200

    config example

--- ibmr/ibmr.js
+++ ibmr/ibmr.js
@@ -1,12 +1,16 @@
 define(['jquery',
+		'config',
 		'ibmr/controllers/FormController',
 		'ibmr/controllers/PersonController'], 
 
-	function($, FormController, PersonController) {
+	function($, config, FormController, PersonController) {
 
 			 var IBMR = {
 			 
 				start:function(){
+
+					$("title").append(config.title);
+
 					FormController.init();
 					PersonController.init();
 				}
