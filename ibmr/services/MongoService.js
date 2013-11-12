e2a09a5500355a0e5545caee09af0050ee294391
moraruetc <moraru.etc@gmail.com>
2013-11-12 14:53:21 +0200

    new mongo collection

--- ibmr/services/MongoService.js
+++ ibmr/services/MongoService.js
@@ -7,7 +7,7 @@ define(['jquery'],
 				saveIbmPerson:function(person){
 					
 
-					return $.ajax( { url: "https://api.mongolab.com/api/1/databases/intouch/collections/pacients?apiKey=DNoaap2Y9z5bHhAGKv2w5ulrtV-L4h6G",
+					return $.ajax( { url: "https://api.mongolab.com/api/1/databases/ibmr/collections/persons?apiKey=DNoaap2Y9z5bHhAGKv2w5ulrtV-L4h6G",
 						type: "POST",
         				data: JSON.stringify( person ),          					
           				contentType: "application/json" } );
