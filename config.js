4a782f65705e15398aa8e338a0a42d1f8745164c
moraruetc <moraru.etc@gmail.com>
2013-12-09 15:10:00 +0200

    no https because of chrome

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 define({
     "title": ":::AMD example:::",
     "mongolab" : "https://api.mongolab.com/api/1/databases/ibmr/collections/persons?apiKey=DNoaap2Y9z5bHhAGKv2w5ulrtV-L4h6G",
-    "ws4d" : "https://istart-rest-dev1.lexington.ibm.com/restws/ebi/iemetrics/5283/"
+    "ws4d" : "http://istart-rest-dev1.lexington.ibm.com/restws/ebi/iemetrics/5283/"
 });
